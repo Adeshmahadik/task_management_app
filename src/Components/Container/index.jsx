824485c5bfabc4a6588d62aa6285907de763ac5d
Adeshmahadik <adeshmahadikcool12@gmail.com>
2024-02-10 20:27:53 +0530

    update: remove tasks

--- src/Components/Container/index.jsx
+++ src/Components/Container/index.jsx
@@ -1,24 +1,34 @@
 import React, { useEffect, useState } from "react";
+import { IoIosRemoveCircleOutline } from "react-icons/io";
 
-const Container = ({ cardData, title }) => {
+const Container = ({ cardData, setdeleteData,  title }) => {
   const [data, setData] = useState(cardData);
+  
 
   useEffect(() => {
     setData(cardData);
   }, [cardData]);
+
+
+
   return (
     <div className="container_section">
       <div className="container_section_child">
         <div className="titleSection">
             <span className="totalnoVal">{data?.length} </span>
             {title}
+
         </div>
         <div className="cardSection">
           {data.map((data, index) => (
             <div className="card" key={data?.id}>
-              <p className="value_and_status">
-                <span>{index +1}</span> {data?.status}
-              </p>
+              <div className="value_and_status flex-row center-between ">
+                <p><span>{index +1}</span> {data?.status}</p>
+                <div className="" onClick={() => (setdeleteData(data.id))}>
+                 <IoIosRemoveCircleOutline />
+                </div>
+        
+              </div>
               <p> {data?.text}</p>
               <p>Valid till : {data?.date} </p>
             </div>
--- src/Pages/MainPage.jsx
+++ src/Pages/MainPage.jsx
@@ -1,15 +1,32 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import "./MainPage.css";
 import ButttonComponent from '../Components/Button';
 import Modal from '../Components/Modal';
 import TaskInput from '../Components/Input';
 import Container from '../Components/Container';
-
+import toast from "react-hot-toast";
 const MainPage = () => {
   const [showModal, setShowModal] = useState(false);
-const AllStoredData = JSON.parse(localStorage.getItem("savedItem"));
-console.log(AllStoredData, "AllStoredData");
-  //define localstorage parameters
+  const [deleteData, setdeleteData] = useState("");
+  const AllStoredData = JSON.parse(localStorage.getItem("savedItem")) || [];
+  console.log(AllStoredData, "AllStoredData");
+
+  const statusTypes = ["todo", "inprogress", "done"];
+
+  const filteredData = statusTypes.map(status => ({
+    status,
+    data: AllStoredData.filter(tasks => tasks.status === status)
+  }));
+  useEffect(() => {
+    if(deleteData !== "") {
+
+      const arrayOfObjects = AllStoredData.filter(obj => obj.id !== deleteData);
+      localStorage.setItem("savedItem", JSON.stringify(arrayOfObjects))
+      toast.success("task removed successfully!")
+      setdeleteData("")
+    }
+  }, [deleteData])
+  
 
   return (
     <div className='MainPage flex-col start-center'>
@@ -17,16 +34,15 @@ console.log(AllStoredData, "AllStoredData");
         <ButttonComponent className={"AddTaskBtn"} text="Add Task" handleClick={() => setShowModal(!showModal)} />
       </div>
       <div className='children_Section'>
-        <Container title="todo" cardData={AllStoredData} />
-        <Container title="In progress" cardData={AllStoredData} />
-        <Container title="Done" cardData={AllStoredData} />
-        
+        {filteredData.map(({ status, data }) => (
+          <Container setdeleteData={setdeleteData} key={status} title={status} cardData={data} />
+        ))}
       </div>
-      {showModal &&
-        <Modal setShowModal={setShowModal} showModal={showModal}>
-          <TaskInput prevTask={AllStoredData ? AllStoredData :[]} setShowModal={setShowModal} />
+      {showModal && (
+        <Modal setShowModal={setShowModal} showModal={showModal} title="Add Task">
+          <TaskInput prevTask={AllStoredData} setShowModal={setShowModal} />
         </Modal>
-      }
+      )}
     </div>
   );
 }
