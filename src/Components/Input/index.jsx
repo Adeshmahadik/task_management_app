f9127ccc239d32728823a9f443d66d09da09bc79
Adeshmahadik <adeshmahadikcool12@gmail.com>
2024-02-10 15:49:42 +0530

    all input data make it through form

--- src/Components/Input/index.jsx
+++ src/Components/Input/index.jsx
@@ -2,70 +2,53 @@ import React, { useState } from 'react';
 import ButttonComponent from '../Button';
 
 const TaskInput = ({ setShowModal }) => {
-  const [taskText, setTaskText] = useState('');
-  const [dueDate, setDueDate] = useState('');
-  const [showErr, setShowErr] = useState(false);
-  const [dateErr, setDateErr] = useState(false);
+  const [taskData, setTaskData] = useState({ id: "", text: "", status: "Todo", date: "" });
 
-  const handleInputChange = (e) => {
-    setTaskText(e.target.value);
-    if (showErr) {
-      setShowErr(false);
-    }
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setTaskData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
   };
 
-  const handleDateChange = (e) => {
-    setDueDate(e.target.value);
-    if (dateErr) {
-      setDateErr(false);
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const currentDate = new Date().toISOString();
+    const newData = {
+      ...taskData,
+      id: currentDate,
+      status: "todo",
+    };
+    setTaskData(newData);
+    console.log(newData);
+    setShowModal(false);
   };
-
-  const handleSaveClick = () => {
-    if (taskText === "") {
-      setShowErr(true);
-    } else if (dueDate === "") {
-      setDateErr(true);
-    } else {
-      // Save task to localStorage
-      const savedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
-      const newTask = { text: taskText, dueDate };
-      savedTasks.push(newTask);
-      localStorage.setItem('tasks', JSON.stringify(savedTasks));
-      
-
-      console.log('Task saved:', newTask);
-      setShowModal(false);
-    }
-  };
-
-  const currentDate = new Date();
+  
 
   return (
-    <div className='add_Task_Section '>
-     
-      <form style={{rowGap: "10px"}} className='flex-col start-start'>
-      <label>Add Task</label>
+    <div className='add_Task_Section'>
+      <form style={{ rowGap: "10px" }} className='flex-col start-start' onSubmit={handleSubmit}>
+        <label>Add Task</label>
         <input
+          name="text"
           type="text"
           placeholder='add your text'
-          value={taskText}
-          onChange={handleInputChange}
-          required  // added required attribute
+          value={taskData.text}
+          onChange={handleChange} 
+          required
         />
-        {showErr && <span className='error'>!Please enter field</span>}
         <label>Due Date</label>
         <input
-        minDate={currentDate}
           type="date"
+          name="date"
           placeholder="select date"
-          value={dueDate}
-          onChange={handleDateChange}
-          required  // added required attribute
+          value={taskData.date}
+          onChange={handleChange}
+          required
         />
-        {dateErr && <span className='error'>!Please select a date</span>}
         <div style={{ width: "98%" }} className='flex-row start-end'>
-          <ButttonComponent type={"submit"} className={"AddTaskBtn"} text="Add Task" handleClick={handleSaveClick} />
+          <ButttonComponent type={"submit"} className={"AddTaskBtn"} text="Add Task" />
         </div>
       </form>
     </div>
